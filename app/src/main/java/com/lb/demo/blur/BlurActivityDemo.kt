792cb03a603634a61c76b90862f82f7bcbb868e8
Alan Lee <augusting1991@163.com>
2023-06-29 23:05:42 +0800

    使用RenderEffect进行高斯模糊

--- app/src/main/java/com/lb/demo/blur/BlurActivityDemo.kt
+++ app/src/main/java/com/lb/demo/blur/BlurActivityDemo.kt
@@ -1,6 +1,9 @@
 package com.lb.demo.blur
 
 import android.graphics.BitmapFactory
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -15,6 +18,7 @@ class BlurActivityDemo: AppCompatActivity() {
 
     private val demoImage by lazy { findViewById<ImageView>(R.id.iv_image_demo) }
     private val btnBlur by lazy { findViewById<Button>(R.id.btn_blur) }
+    private val btnBlur2 by lazy { findViewById<Button>(R.id.btn_blur2) }
     private val btnReset by lazy { findViewById<Button>(R.id.btn_reset) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +29,22 @@ class BlurActivityDemo: AppCompatActivity() {
 
     private fun initView() {
         demoImage.setImageResource(R.drawable.blur_test)
+        btnBlur2.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val blurEffect = RenderEffect.createBlurEffect(0F,
+                    1.0F, Shader.TileMode.CLAMP)
+                demoImage.setRenderEffect(blurEffect)
+            }
+        }
         btnBlur.setOnClickListener {
             val srcBitmap = BitmapFactory.decodeResource(resources, R.drawable.blur_test)
             demoImage.setImageBitmap(BlurUtils.blur(this, srcBitmap))
         }
         btnReset.setOnClickListener {
             demoImage.setImageResource(R.drawable.blur_test)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                demoImage.setRenderEffect(null)
+            }
         }
     }
 }
